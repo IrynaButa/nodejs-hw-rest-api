817ffbe57a5dd1003eb5b8b19a2b880ca539f9e9
Iryna Dagli <dagliirina@ukr.net>
2021-08-28 14:29:29 +0300

    add controllers

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,70 +1,14 @@
 const express = require('express')
 const router = express.Router()
-const Contact = require('../../model');
+const ctrl = require('../../controllers/contactsCtrl')
 const validate = require('./validation')
 
-router.get('/', async (_req, res, next) => {
-  try {
-    const contacts = await Contact.listContacts();
-    return res.json({ status: 'success', code: 200, data: { contacts }  });
-  } catch (error) {
-    next();
-  }
-  
-  
-})
 
-router.get('/:contactId', async (req, res, next) => {
-  try {
-    const contact = await Contact.getContactById(req.params.contactId);
-    if (!contact) {
-      return res.status(400).json({ message: 'Not found' });
-    }
-    return res.status(201).json({ status: 'success', code: 201, data: { contact }  })
-  } catch (error) {
-    next(error);
-  }
-  
+router.get('/', ctrl.listContacts ).post('/', validate.addContact, ctrl.addContact )
 
-})
-
-router.post('/', validate.addContact, async (req, res, next) => {
-  try {
-    const contact = await Contact.addContact(req.body);
-    if (!contact) {
-      return res.status(400).json({ message: 'missing required name field' });
-    }
-    return res.status(201).json({ status: 'success', code: 201, data: { contact }  })
-  } catch (error) {
-    next(error);
-  }
-  
-})
-
-router.delete('/:contactId', async (req, res, next) => {
- try {
-    const contact = await Contact.removeContact(req.params.contactId);
-    if (contact) {
-       return res.status(200).json({status: 'success', code: 201, data: { contact }  });
-      
-    }
-   return res.status(404).json({ message: 'Not found' });
-  } catch (error) {
-    next(error);
-  }
-})
-
-router.put('/:contactId', validate.updateContact, async (req, res, next) => {
-  try {
-    const contact = await Contact.updateContact(req.params.contactId, req.body);
-    if (contact) {
-       return res.status(200).json({status: 'success', code: 201, data: { contact }  });
-      
-    }
-   return res.status(404).json({ message: 'Not found' });
-  } catch (error) {
-    next(error);
-  }
-})
+router
+  .get('/:contactId', ctrl.getContactById)
+  .delete('/:contactId', ctrl.removeContact)
+  .put('/:contactId', validate.updateContact, ctrl.updateContact)
 
 module.exports = router
