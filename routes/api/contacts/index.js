005226703b2224c3892b393b5d54bddcc881041b
Iryna Dagli <dagliirina@ukr.net>
2021-09-05 17:34:02 +0300

    routes token

--- routes/api/contacts/index.js
+++ routes/api/contacts/index.js
@@ -2,15 +2,17 @@ const express = require('express')
 const router = express.Router()
 const ctrl = require('../../../controllers/contactsCtrl')
 const validate = require('./validation')
+const guard = require('../../../helper/guard');
+
 
 router.get('/', ctrl.listContacts).post('/', validate.addContact, ctrl.addContact)
 
 router
-  .get('/:contactId', validate.validateMongoId, ctrl.getContactById)
-  .delete('/:contactId', validate.validateMongoId, ctrl.removeContact)
-  .put('/:contactId', validate.updateContact, ctrl.updateContact)
+  .get('/:contactId', guard, validate.validateMongoId, ctrl.getContactById)
+  .delete('/:contactId',guard, validate.validateMongoId, ctrl.removeContact)
+  .put('/:contactId',guard, validate.updateContact, ctrl.updateContact)
 
 router
-  .patch('/:contactId/favorite', validate.validateMongoId, ctrl.getFavorite)
+  .patch('/:contactId/favorite', guard, validate.validateMongoId, ctrl.getFavorite)
 
 module.exports = router
--- routes/api/users/index.js
+++ routes/api/users/index.js
@@ -3,16 +3,15 @@ const router = express.Router()
 const ctrl = require('../../../controllers/usersCtrl')
 //const validate = require('./validation')
 
-router.post('/users/signup', ctrl.signup)
-router.post('/users/login',  ctrl.login)
-router.post('/users/logout',  ctrl.logout)
+router.post('/signup', ctrl.signup)
+router.post('/login',  ctrl.login)
+router.post('/logout',  ctrl.logout)
 
-// router
-//   .get('/:contactId', validate.validateMongoId, ctrl.getContactById)
-//   .delete('/:contactId', validate.validateMongoId, ctrl.removeContact)
-//   .put('/:contactId', validate.updateContact, ctrl.updateContact)
+router.get('/users/current', ctrl.checkUserByToken);
 
-// router
-//   .patch('/:contactId/favorite', validate.validateMongoId, ctrl.getFavorite)
+router.patch(
+  '/users',
+  ctrl.updateUserSubscription
+);
 
 module.exports = router
